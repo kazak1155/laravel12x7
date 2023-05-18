97ce44030a61cdbf97e5a0e6d9695e781a00adaf
Andrey Volynkin <kazakmt4@gmail.com>
2023-05-18 09:05:27 +0300

    почему-то не все файлы отслеживались репозиторием

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -52,7 +52,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
+        $product->delete();
         return response()->json($product,204);
-
     }
 }
